33c2f7c1bab3ed07a1301f8bb88aaee9acd8ee1e
emilydion <143626936+edion-heco@users.noreply.github.com>
2024-09-10 15:34:49 -0500

    added popups, hover effects, corrected points

--- js/leaflet-map.js
+++ js/leaflet-map.js
@@ -14,20 +14,124 @@ var imageBounds = [[100, -200], [-100, 200]]; //determines image centering place
 L.imageOverlay(imageUrl, imageBounds).addTo(map); 
 
 
-//Adding leaflet layers
+//Begin adding leaflet layers
 // Example polygon: var polygon = L.polygon([[-37, 43], [37, 43], [0, 4.7]], {color: '#00C00C', fillColor:'#00C00C', fillOpacity: 0.5}).addTo(map);
 
-let radars = new Map([
-    ["Front Facing Camera", [[-36, 86], [36, 86], [0, 9.2]]],
-    ["Second Radar", [[-60, -20], [40, -0], [0, -50]]],
-]);
+//added using Mapped values
+// let radars = new Map([
+//     ["Front Facing Camera", [[-36, 86], [36, 86], [0, 9.2]]],
+//     ["Second Radar", [[-60, -20], [40, -0], [0, -50]]],
+// ]);
 
-for (let [key, latlngs] of radars) {
-    var polygon = L.polygon(latlngs, {color: '#00C00C', fillColor:'#00C00C', fillOpacity: 0.25}).addTo(map);
-    polygon.bindPopup("I am a " + key);
-    // polygon.on('mouseover', function(e) {
-    //     ({color: '#C00C00'})
-    // };
+// looping through mapped values
+// for (let [key, latlngs] of radars) {
+//     var polygon = L.polygon(latlngs, {color: '#00C00C', fillColor:'#00C00C', fillOpacity: 0.25}).addTo(map);
+//     polygon.bindPopup("I am a " + key);
+//     // polygon.on('mouseover', function(e) {
+//     //     ({color: '#C00C00'})
+//     // };
+// }
+
+//OR added using GeoJSON format
+
+var adas = [{
+    "type" : "Feature", 
+    "properties": {
+        "adasType": "Radar",
+        "name": "Front Facing Radar",
+        "description": "A temporary short sentence about front facing radar technology.",
+        "show_on_map": true
+    }, 
+    "geometry": {
+        "type": "Polygon", 
+        "coordinates": [[
+            [86, 36],
+            [86, -36],
+            [9.2, 0]
+        ]]
+    }
+}, {
+    "type" : "Feature", 
+    "properties": {
+        "adasType": "Camera",
+        "name": "Rear Facing Camera",
+        "description": "A temporary short sentence about rear facing camera technology.",
+        "show_on_map": true
+    }, 
+    "geometry": {
+        "type": "Polygon", 
+        "coordinates": [[
+            [-86, 36],
+            [-50, 0],
+            [-86, -36]
+        ]]
+    }
+}];
+
+// L.geoJSON(adas, {
+//     style: function(feature) {
+//         switch (feature.properties.adasType) {
+//             case 'Radar': return {color: "#00C00C"};
+//             case 'Camera':   return {color: "#C00C00"};
+//         }
+//     }
+// }).addTo(map);
+
+var geojson; 
+
+function getColor(type) {
+    return type == "Camera" ? '#C00C00' :
+           type == "Radar"  ? '#00C00C' :
+                      '#000000';
 }
+
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.adasType),
+        color: getColor(feature.properties.adasType),
+        weight: 2,
+        fillOpacity: 0.25
+    };
+}
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        fillOpacity: 0.5
+    });
+
+    layer.bringToFront();
+}
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+}
+
+// function zoomToFeature(e) {
+//     map.fitBounds(e.target.getBounds());
+// }
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        // click: zoomToFeature
+    });
+    layer.bindPopup('<h1>'+ feature.properties.name + '</h1>&nbsp;<p>' + feature.properties.description + '</p>')
+}
+
+var geojson = L.geoJson(adas, {
+    filter: function(feature, layer) {
+        return feature.properties.show_on_map;
+    },
+    style: style,
+    onEachFeature: onEachFeature,
+}).addTo(map);
+
+
+
+
 
 
